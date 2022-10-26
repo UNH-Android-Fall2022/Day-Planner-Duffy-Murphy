9fd8fd3e5173141c391ae7d9f995a43a3be86884
orionduffy <45501204+orionduffy@users.noreply.github.com>
2022-10-26 16:02:47 -0400

    Minor refactor
    I forgot something in the last refactor

--- app/src/main/java/com/example/dayplanner/ui/planner/PlannerFragment.kt
+++ app/src/main/java/com/example/dayplanner/ui/planner/PlannerFragment.kt
@@ -28,7 +28,7 @@ class PlannerFragment : Fragment() {
         _binding = FragmentPlannerBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
+        val textView: TextView = binding.textPlanner
         plannerViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
--- app/src/main/java/com/example/dayplanner/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/dayplanner/ui/settings/SettingsFragment.kt
@@ -28,7 +28,7 @@ class SettingsFragment : Fragment() {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
+        val textView: TextView = binding.textSettings
         settingsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }

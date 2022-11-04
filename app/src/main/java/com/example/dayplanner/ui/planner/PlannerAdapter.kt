5006cc520746f3f313d800c715d6b1d68b8a535b
orionduffy <45501204+orionduffy@users.noreply.github.com>
2022-11-03 20:59:03 -0400

    More progress on planner recycler view

--- app/src/main/java/com/example/dayplanner/ui/planner/PlannerAdapter.kt
+++ app/src/main/java/com/example/dayplanner/ui/planner/PlannerAdapter.kt
@@ -1,5 +1,46 @@
 package com.example.dayplanner.ui.planner
 
+import android.content.ContentValues
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dayplanner.R
 
-class PlannerAdapter (private val eventList: ArrayList<PlannerItem>){
+
+class PlannerAdapter (
+    private val eventList: ArrayList<PlannerItem>,
+    private val context: PlannerFragment
+    ): RecyclerView.Adapter<PlannerAdapter.ViewHolder>(){
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val startTime: TextView = itemView.findViewById(R.id.start_time)
+        val endTime: TextView = itemView.findViewById(R.id.end_time)
+        val name: TextView = itemView.findViewById(R.id.event_name)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.planner_item, parent, false)
+
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int){
+        val (startTime, endTime, name) = eventList[position]
+        //TODO("Implement image resource")
+        holder.startTime.text = startTime
+        holder.endTime.text = endTime
+        holder.name.text = name
+
+        holder.itemView.setOnClickListener {
+            Log.d(ContentValues.TAG, "Position clicked is $position")
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return eventList.size
+    }
 }
--- app/src/main/java/com/example/dayplanner/ui/planner/PlannerFragment.kt
+++ app/src/main/java/com/example/dayplanner/ui/planner/PlannerFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.dayplanner.data.eventList
 import com.example.dayplanner.databinding.FragmentPlannerBinding
 
 class PlannerFragment : Fragment() {
@@ -27,6 +29,23 @@ class PlannerFragment : Fragment() {
 
         _binding = FragmentPlannerBinding.inflate(inflater, container, false)
         val root: View = binding.root
+// TODO Fix this
+//        val eventRecyclerList: ArrayList<PlannerItem> = ArrayList()
+//
+//        for(item in eventList){
+//            eventRecyclerList.add(
+//                PlannerItem(
+//                    item.startTime,
+//                    workout.name,
+//                    workout.workoutTime.toString()+" Minutes"
+//                )
+//            )
+//        }
+//
+//        mRecyclerView = binding.recyclerViewWorkouts
+//        mRecyclerView.setHasFixedSize(true)
+//        mRecyclerView.layoutManager = LinearLayoutManager(context)
+//        mRecyclerView.adapter = WorkoutAdapter(workoutRecyclerList, this)
 
         return root
     }
